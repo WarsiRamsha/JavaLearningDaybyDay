ce4852954eb87c1ba2cfc19629a0480e89729112
Ramsha <ramsha_warsi@hotmail.com>
2024-02-28 14:04:47 +0500

    Swapping a number and sorting an array

---
+++ src/JavaQuestions/CorrectedSwapTwoVariables.java
@@ -0,0 +1,20 @@
+package JavaQuestions;
+
+public class CorrectedSwapTwoVariables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		        int b = 10;
+		        
+		        int temp = a;
+		        a = b;
+		        b = temp;
+		        
+		        System.out.println("After swapping:");
+		        System.out.println("a = " + a);
+		        System.out.println("b = " + b);
+		    }
+		}
+
+
---
+++ src/JavaQuestions/SortingArray.java
@@ -0,0 +1,27 @@
+package JavaQuestions;
+
+import java.awt.FocusTraversalPolicy;
+
+public class SortingArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[] = {11,9,5,7,3, 6,2};
+		int swappedNumber = 0;
+		for(int i=0; i<=a.length; i++) {
+			for(int j = i+1; j<a.length; j++) {
+				if(a[i] > a[j]) {
+					swappedNumber=a[i]; 
+					a[i]= a[j];
+					a[j]=swappedNumber;
+			}
+			
+		
+			}
+		}
+		for(int i =0; i<a.length-1; i++) {
+			System.out.println(a[i]);
+		}
+	}
+
+}
