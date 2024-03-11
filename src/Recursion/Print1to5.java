e3a2a4f0f99f13bfdf4a713cb8559bbae3011879
Ramsha <ramsha_warsi@hotmail.com>
2024-03-11 22:04:35 +0500

    changes made

---
+++ src/Recursion/Print1to5.java
@@ -0,0 +1,23 @@
+package Recursion;
+
+public class Print1to5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 1;
+		toPrint1to5(n);
+
+	}
+
+	private static void toPrint1to5(int n) {
+		// TODO Auto-generated method stub
+		if(n>10) {
+			return;
+		}
+		
+		System.out.println(n);
+		toPrint1to5(n+1);
+		
+	}
+
+}
