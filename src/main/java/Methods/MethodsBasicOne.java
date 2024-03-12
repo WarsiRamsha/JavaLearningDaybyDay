d24b9f7d9c1c59b70083338e9668129574643c4c
Ramsha <ramsha_warsi@hotmail.com>
2024-03-12 13:22:25 +0500

    Updates in Pom

---
+++ src/main/java/Methods/MethodsBasicOne.java
@@ -0,0 +1,14 @@
+package Methods;
+
+public class MethodsBasicOne {
+
+	public MethodsBasicOne() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
