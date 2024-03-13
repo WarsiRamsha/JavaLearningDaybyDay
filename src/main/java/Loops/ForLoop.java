fd7f799c4e3ac96a10255b6f5bb4355a1d9830bd
Ramsha <ramsha_warsi@hotmail.com>
2024-03-13 10:28:39 +0500

    Created a Basic Method and loops class

---
+++ src/main/java/Loops/ForLoop.java
@@ -0,0 +1,20 @@
+package Loops;
+
+public class ForLoop {
+
+	public ForLoop() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//print table of 2
+		int table =2;
+		for(int i=1; i<=10; i++) {
+			int answer= table*i;
+			System.out.println(answer);
+			
+		}
+	}
+
+}
---
+++ src/main/java/Methods/MethodsValueIsSent.java
@@ -0,0 +1,17 @@
+package Methods;
+public class MethodsValueIsSent {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int c = 10;
+		int d= 5;
+		int answer = toGetSum(c,d);
+		System.out.println(answer);
+		
+
+	}
+	public static int toGetSum(int a , int b) {
+		int sum = a +b;
+		return sum;
+	}
+
+}
