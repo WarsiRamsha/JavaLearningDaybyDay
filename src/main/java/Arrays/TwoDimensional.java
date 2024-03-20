2e3b3d6fd07773b12bcce1920a8815df0b9f4942
Ramsha <ramsha_warsi@hotmail.com>
2024-03-20 20:03:56 +0500

    Added arrays package

---
+++ src/main/java/Arrays/TwoDimensional.java
@@ -0,0 +1,38 @@
+package Arrays;
+
+public class TwoDimensional {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int [][] TwoDim;
+		TwoDim = new int [3][3] ;			//pressed Tab three times declaring array
+		
+		int a = 15;
+		
+		for (int i = 0; i <  3; i++ ){
+			for (int j = 0; j < 3; j++) {
+				TwoDim[i][j] = a;
+				a += 5;
+			}
+		}		
+		for (int i = 0; i <  3; i++ ){
+			for (int j = 0; j < 3; j++) {
+				System.out.println(TwoDim[i][j]);
+			}
+		}		
+		
+	}
+}
+	/*for (int i = 0; i <  3; i++ )
+	{
+		for (int j = 0; j < 3; j++);
+	}
+	{
+		System.out.println(TwoDim[2][2]);
+	}
+		/*int [] R= {1,2,3,4,5,6,7,8,9,10};
+		for (int ramp: R)
+		{
+			System.out.println(ramp);
+		}*/
