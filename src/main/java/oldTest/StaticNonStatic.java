9f622cad8bb52ec32b5b04de81f839acb7a65604
Ramsha <ramsha_warsi@hotmail.com>
2024-04-08 20:08:10 +0500

    Difference between static and nonstatic methods and or fields

---
+++ src/main/java/oldTest/StaticNonStatic.java
@@ -0,0 +1,17 @@
+package oldTest;
+
+public class StaticNonStatic {
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StaticNonStaticII.carniVore();
+		StaticNonStaticII.herbiVore();
+		StaticNonStaticII.omniVore();
+		StaticNonStaticII statNonStaticII = new StaticNonStaticII();
+		System.out.println(statNonStaticII.colour= "brown" );
+		
+	}
+
+}
